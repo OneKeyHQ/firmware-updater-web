f00c19b2fb1ec00d584309393222620447f35864
Leon <lixiao.dev@gmail.com>
2023-06-16 16:32:22 +0800

    chore: update version

--- src/hardware/instance.ts
+++ src/hardware/instance.ts
@@ -18,7 +18,7 @@ export const getHardwareSDKInstance = memoizee(
       const settings: Partial<ConnectSettings> = {
         debug: true,
         connectSrc: 'https://jssdk.onekey.so/0.3.11/',
-        preRelease: true,
+        preRelease: false,
       };
 
       try {
