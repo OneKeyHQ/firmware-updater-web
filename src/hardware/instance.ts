f9fa8bbd793ce596a3d7365f75ad20ba681d1de6
ByteZhang <quincysx@onekey.so>
2024-03-21 10:46:55 +0800

    chore: add i18n

--- src/hardware/instance.ts
+++ src/hardware/instance.ts
@@ -18,7 +18,7 @@ export const getHardwareSDKInstance = memoizee(
       const settings: Partial<ConnectSettings> = {
         debug: true,
         connectSrc: 'https://jssdk.onekey.so/0.3.41/',
-        preRelease: true,
+        preRelease: false,
         fetchConfig: true,
       };
 
