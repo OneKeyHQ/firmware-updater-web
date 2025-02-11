7033cc486903a0ad9017e345b0e4809d776da1bd
wabiwabo <68363074+wabicai@users.noreply.github.com>
2025-02-11 22:48:55 +0800

    feat: add meta i18n (#47)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,38 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from '@/components';
 // import './App.css';
 import '@onekeyfe/ui-components/index.css';
+import { useIntl } from 'react-intl';
+import { useEffect } from 'react';
 import Dashboard from './views/Dashboard';
 
 export default function App() {
+  const intl = useIntl();
+
+  useEffect(() => {
+    const title = intl.formatMessage({ id: 'META_TITLE' });
+    const description = intl.formatMessage({ id: 'META_DESCRIPTION' });
+
+    process.env.REACT_APP_META_TITLE = title;
+    process.env.REACT_APP_META_DESCRIPTION = description;
+
+    document.title = title;
+
+    document
+      .querySelector('meta[name="description"]')
+      ?.setAttribute('content', description);
+    document
+      .querySelector('meta[name="og:title"]')
+      ?.setAttribute('content', title);
+    document
+      .querySelector('meta[property="og:description"]')
+      ?.setAttribute('content', description);
+    document
+      .querySelector('meta[name="twitter:title"]')
+      ?.setAttribute('content', title);
+    document
+      .querySelector('meta[property="twitter:description"]')
+      ?.setAttribute('content', description);
+  }, [intl]);
   return (
     <Layout>
       <Router>
