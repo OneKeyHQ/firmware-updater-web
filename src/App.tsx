4160d6f04cc55f40839acd36be0ecea9412bf30e
蔡凯升 <kaisheng.cai@onekey.so>
2025-02-11 15:15:49 +0800

    chore: delete unuse code

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,15 @@ import Dashboard from './views/Dashboard';
 export default function App() {
   const intl = useIntl();
 
-  // 在组件挂载时设置 meta 信息
   useEffect(() => {
     const title = intl.formatMessage({ id: 'META_TITLE' });
     const description = intl.formatMessage({ id: 'META_DESCRIPTION' });
 
-    // 更新环境变量
     process.env.REACT_APP_META_TITLE = title;
     process.env.REACT_APP_META_DESCRIPTION = description;
 
-    // 直接更新 document title
     document.title = title;
 
-    // 更新 meta 标签
     document
       .querySelector('meta[name="description"]')
       ?.setAttribute('content', description);
