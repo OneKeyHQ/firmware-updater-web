945f1edac2c7b60be79b40ec4b1c6e8c73f12f96
蔡凯升 <kaisheng.cai@onekey.so>
2025-09-17 16:48:03 +0800

    fix: upgrade sdk 1.1.13 & fix security issue

--- src/hardware/instance.ts
+++ src/hardware/instance.ts
@@ -17,7 +17,7 @@ export const getHardwareSDKInstance = memoizee(
       }
       const settings: Partial<ConnectSettings> = {
         debug: true,
-        connectSrc: 'https://jssdk.onekey.so/1.1.11/',
+        connectSrc: 'https://jssdk.onekey.so/1.1.13/',
         preRelease: false,
         fetchConfig: true,
       };
