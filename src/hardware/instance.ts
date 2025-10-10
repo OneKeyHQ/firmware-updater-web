7882efd7d60e6dd92884892ca5c7b911bb833f63
wabiwabo <68363074+wabicai@users.noreply.github.com>
2025-10-10 11:47:04 +0800

    fix: 1.1.15-alpha.0 support emmc filewrite in boot (#93)

--- src/hardware/instance.ts
+++ src/hardware/instance.ts
@@ -17,7 +17,7 @@ export const getHardwareSDKInstance = memoizee(
       }
       const settings: Partial<ConnectSettings> = {
         debug: true,
-        connectSrc: 'https://jssdk.onekey.so/1.1.13/',
+        connectSrc: 'https://jssdk.onekey.so/1.1.15/',
         preRelease: false,
         fetchConfig: true,
       };
